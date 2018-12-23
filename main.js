ee8727e3977560e1b652e04421846ff2d1bcc281
Hannah Neal <hannahmneal@gmail.com>
2018-12-22 19:34:18 -0600

    completed exercise

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 console.log("test");
+
+// Create an object that represents your pet.
+
+// 1. Name property with a string value.
+// 2. Species property with a string value.
+// 3. Nicknames property with an array value. Array contains strings.
+// 4. Age property with an integer value
+
+let myPet = {
+    name: "Clove",
+    species: "Felis silvestris",
+    nicknames: [
+        "snotty", "tiny cat", "sniffly cat", "sneezy"
+    ],
+    age: 3
+
+}
+
+console.log(myPet);
+
